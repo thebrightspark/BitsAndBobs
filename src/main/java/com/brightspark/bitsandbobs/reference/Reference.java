312fe70ddda78e23cf090fbf6a7716d7944b2da2
bright_spark <markpassey@btinternet.com>
2016-09-21 23:47:30 +0100

    Updated version to 1.10.2-1.4

--- src/main/java/com/brightspark/bitsandbobs/reference/Reference.java
+++ src/main/java/com/brightspark/bitsandbobs/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "bitsandbobs";
     public static final String MOD_NAME = "Bits And Bobs";
-    public static final String VERSION = "1.10.2-1.3";
+    public static final String VERSION = "1.10.2-1.4";
     public static final String DEPEND =
             "after:" + Names.ModIds.ENDERIO +
             ";after:" + Names.ModIds.OPEN_BLOCKS;
